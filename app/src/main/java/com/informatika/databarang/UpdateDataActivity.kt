d2aed7a36aeeed09b3285b05e49cb4243a6a5da1
jinxnx <fikriarsyad27@gmail.com>
2021-06-28 16:14:15 +0700

    dataBarang

---
+++ app/src/main/java/com/informatika/databarang/UpdateDataActivity.kt
@@ -0,0 +1,11 @@
+package com.informatika.databarang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_data)
+    }
+}
